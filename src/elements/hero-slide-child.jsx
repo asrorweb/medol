47f54f75395ea71b7b3e802328e62645c41b3073
asrorweb <asrorakmalovich2021@gmail.com>
2023-11-26 22:59:38 +0500

    hero

--- src/elements/hero-slide-child.jsx
+++ src/elements/hero-slide-child.jsx
@@ -7,7 +7,7 @@ function HeroSlideChild({ data }) {
     return (
         <div className="max-w-[1430px] mb-[108px] max-md:px-[20px] max-xl:mb-[40px] max-md:mb-[30px]  pt-[119px] max-xl:pt-[106px] max-md:pt-[53px] mx-auto px-[35px] flex justify-between items-center">
             <div>
-                <h1 className="text-[#0D4C93] break-words  font-medium text-[50px] tracking-[4.5px] text-left mb-[31px] max-md:text-[32px] max-md:mb-[22px] max-md:tracking-[2.88px]">
+                <h1 className="text-[#0D4C93] whitespace-pre-wrap  font-medium text-[50px] tracking-[4.5px] text-left mb-[31px] max-md:text-[32px] max-md:mb-[22px] max-md:tracking-[2.88px]">
                     {data.title}
                 </h1>
                 <p className="text-[#363535] text-[24px] leading-[40px] max-w-[680px] text-left mb-[43px] max-md:text-[20px] max-md:leading-[30px] max-md:mb-[37px]">
